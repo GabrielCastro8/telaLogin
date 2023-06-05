cdcabc20df37722f29d017ea181d6b0808ded73a
GabrielCastro8 <gabriel.aldc7@gmail.com>
2023-06-05 04:25:06 -0300

    Tela de login

---
+++ Usabilidade A3 - Copia/src/routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const routes = express.Router();
+
+const users= [{
+    id:1,
+    name: 'Gabriel',
+    email: 'meuemail@gmail.com',
+    password: '12345'
+}];
+
+routes.post('/login', (req, res) =>{
+    const {email,password} = req.body;
+
+    const user = users.find(user => user.email === email && user.password===password);
+
+    if(user){
+        return res.status(200).json(user);
+    }
+
+    else{
+        return res.status(401).json({message: 'Informacoes invalidas'});
+    }
+
+    
+});
+
+module.exports = routes;
